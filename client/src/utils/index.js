7ebfcda6548293bbe3a724943f8be14a8fc634ab
Ashwin Sreekumaran Nair <sreeaswin.98@gmail.com>
2023-02-14 23:29:58 +0530

    first commit

---
+++ client/src/utils/index.js
@@ -0,0 +1,14 @@
+
+import FileSaver from 'file-saver';
+import {surpriseMePrompts} from '../constants';
+
+export function getRandomPrompt(prompt) {
+    const randomIndex = Math.floor(Math.random()*surpriseMePrompts.length);
+    const getRandomPrompt = surpriseMePrompts[randomIndex];
+    if(getRandomPrompt === prompt) return getRandomPrompt(prompt);
+    return getRandomPrompt;
+}
+
+export async function downloadImage(_id,photo) {
+    FileSaver.saveAs(photo,`download-${_id}.jpg`);
+}
